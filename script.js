4cfcfb38c454a7a95ea0da7fddd4e21e010b1a9d
smntsng <144932270+smntsng@users.noreply.github.com>
2023-11-20 14:24:32 +0000

    corrected time blockcode to show PM hours

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-// Display the current day at the top of the calender when a user opens the planner.
-
-// Present timeblocks for standard business hours when the user scrolls down.
-
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 
 // Allow a user to enter an event when they click a timeblock
 
 // Save the event in local storage when the save button is clicked in that timeblock.
 
 // Persist events between refreshes of a page
-// Uses a date utility library to work with date and time
 
-// 1. add current time to the header of website using jquery 
-    // a. select existing dom element var rootEl = $('#pikachu');
-    // create a var that holds the date and time code
-    // create a new element to a assign new var
-    // append rootEl.append(quoteEl);
-// 2. create the time blocks using flex or gird that are only available during business hours
-// 3. colour code timeblocks based on time
-    // rootEl.children('ul').children().css('background-color', 'white');
-    // ::hover?
+
 // 3. make the timeblocks clickable 
     // add a lister for the click
     // funtion to create a 
@@ -46,14 +32,15 @@ function displayTime() {
 //   calling the function displayTime excute this code ever 1000ms
 setInterval(displayTime,1000);
 
-var textareaEL = document.getElementById("textarea")
-var saveBtnEL = document.getElementById("saveBtn")
+// var userInput1EL = document.getElementById("userInput1")
+// var saveBtnEL = document.querySelector(".saveBtn")
 
-// textareaEL.value = localStorage.getItem('userInput1');
-// saveBtnEL.addEventListener('click', updateUserInput1);
-// //sets the input of text area into local storage
+// saveBtnEL.addEventListener("click", function(e) {
+//     console.log(e)
+// });
+
 // function updateUserInput1() {
-//     localStorage.setItem('userInput1', textareaEL.value);
+//     localStorage.setItem('userInput1', userInput1EL.value);
 // };
 
 
@@ -93,42 +80,42 @@ if (currentHour < 12){
     $(".12").addClass("past");
 }
 
-if (currentHour < 13){
+if (currentHour < 1){
     $(".13").addClass("future");
-} else if(currentHour == 13){
+} else if(currentHour == 1){
     $(".13").addClass("present");
-}else if(currentHour > 13){
+}else if(currentHour > 1){
     $(".13").addClass("past");
 }
 
-if (currentHour < 14){
+if (currentHour < 2){
     $(".14").addClass("future");
-} else if(currentHour == 14){
+} else if(currentHour == 2){
     $(".14").addClass("present");
-}else if(currentHour > 14){
+}else if(currentHour > 2){
     $(".14").addClass("past");
 }
 
-if (currentHour < 15){
+if (currentHour < 3){
     $(".15").addClass("future");
-} else if(currentHour == 15){
+} else if(currentHour == 3){
     $(".15").addClass("present");
-}else if(currentHour > 15){
+}else if(currentHour > 3){
     $(".15").addClass("past");
 }
 
-if (currentHour < 16){
+if (currentHour < 4){
     $(".16").addClass("future");
-} else if(currentHour == 16){
+} else if(currentHour == 4){
     $(".16").addClass("present");
-}else if(currentHour > 16){
+}else if(currentHour > 4){
     $(".16").addClass("past");
 }
 
-if (currentHour < 17){
+if (currentHour < 5){
     $(".17").addClass("future");
-} else if(currentHour == 17){
+} else if(currentHour == 5){
     $(".17").addClass("present");
-}else if(currentHour > 17){
+}else if(currentHour > 5){
     $(".17").addClass("past");
 }
